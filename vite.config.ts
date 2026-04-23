36ee0be1fda505787e900d0d60ee362f12c8f9ec
philip <mr.philipsalvador@gmail.com>
2026-04-23 23:02:22 +0800

    Fixed the Vite

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-  assetsInclude: ['**/*.svg', '**/*.csv', '**/*.glb', '**/*.gltf'], // <-- ADDED 3D MODELS HERE
 })
